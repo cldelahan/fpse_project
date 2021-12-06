5504ca1af6fe0126c226553c87c92362332e7b9a
cdelahan <cldelahan@gmail.com>
2021-12-06 13:50:42 -0500

    add test cases

--- tests/tests.ml
+++ tests/tests.ml
@@ -1,8 +1,34 @@
 (*
-  Put the tests for lib.ml functions here
+  Testing the Graph module
+  Author: Conner Delahanty
+  Date: 12/6/21
 *)
 
-(* open Core;;
-open OUnit2;; *)
+open Core;;
+open OUnit2;;
+open Graph;;
 
-(* TODO *)
+let instance = Broql.empty;;
+let n = Node.empty;;
+let e = Edge.empty;;
+let r = Relation.empty;;
+
+let test_creation _ =
+  assert_equal 0 @@ Broql.n_queries instance;;
+  assert_equal "" @@ Node.to_string n;;
+;;
+
+
+let instantiation_tests =
+  "Instantiation Tests" >: test_list [
+    "Test Create" >:: test_creation;
+  ]
+
+
+let series =
+  "Broql Tests" >::: [
+    instantiation_tests;
+  ]
+
+let () =
+  run_test_tt_main series
