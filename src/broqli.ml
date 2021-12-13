8963cc6a0c929ab90bff6f31fd8f6cd2fea47876
Vinicius Lepca <vinicius.lepca@gmail.com>
2021-12-13 00:16:12 -0500

    Add helper message to the interpreter

--- src/broqli.ml
+++ src/broqli.ml
@@ -58,6 +58,57 @@ let toplevel_loop verbose =
    Format.printf "%a\n" Fbdk.Pp.pp_expr result;
    Format.print_flush () *)
 
+type operation_usage = {name: string; description: string; example: string}
+let operations = [
+  {
+    name = "NODE";
+    description = "Create a node with key-value data pairs.";
+    example = "NODE n1 = {name: \"Conner\", age: \"22\"};"
+  };
+  {
+    name = "RELATION";
+    description = "Add a relation for certain nodes. Relations can be undirected or directed.";
+    example = "RELATION roommates FOR n1, n2, n3;\nRELATION manages DIR FOR n1, n2, n3;"
+  };
+  {
+    name = "ATTR";
+    description = "Get a specific attribute from a node.";
+    example = "ATTR name n1;"
+  };
+  {
+    name = "SEARCH";
+    description = "Get all nodes that meet a certain search criterion.";
+    example = "SEARCH {name: \"Conner\"};"
+  };
+  {
+    name = "WHO";
+    description = "Get nodes that satisfy a certain relation for a node.";
+    example = "WHO roommates FOR n1;"
+  };
+  {
+    name = "SIZE";
+    description = "Get size of a list of nodes.";
+    example = "SIZE WHO roommates FOR n1;"
+  };
+  {
+    name = "LOAD";
+    description = "Load a stored database file (.broql).";
+    example = "LOAD db.broql"
+  };
+  {
+    name = "SAVE";
+    description = "Save the state of the database to a file (.broql).";
+    example = "SAVE db.broql"
+  }
+]
+
+let operations_usage_string (operations: operation_usage list): string =
+  Core.List.fold operations ~init:"" ~f:(fun accum op -> accum ^ op.name ^ ": " ^ op.description ^ "\nEx: " ^ op.example ^ "\n\n")
+
+let usage_str =
+  let basic_usage = "Usage: broqli.exe [--verbose]" in
+  let operations_usage = operations_usage_string operations in
+  basic_usage ^ "\n\nOPERATIONS:\n" ^ operations_usage ^ "COMMAND LINE OPTIONS:"
 
 let main () = 
   let verbose = ref false in
@@ -67,7 +118,7 @@ let main () =
        "Output debug information")
      ])
     (fun _ -> ())
-    ("Usage: broqli.exe [--verbose]");
+    usage_str;
   toplevel_loop !verbose
 
 let () = 
